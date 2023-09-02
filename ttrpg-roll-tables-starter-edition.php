ef2b7c70ffae1546d8c565385b03a46f03117adf
Hugh Lashbrooke <1543143+hlashbrooke@users.noreply.github.com>
2023-09-02 21:26:10 +1200

    Making sure Starter and full versions don't conflict

--- ttrpg-roll-tables-starter-edition.php
+++ ttrpg-roll-tables-starter-edition.php
@@ -20,41 +20,52 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-// Include plugin files
-include 'includes/post-types.php';
-include 'includes/metabox.php';
-include 'includes/ajax.php';
-include 'includes/shortcode.php';
-
-// Set up the plugin
-add_action( 'init', 'trt_plugin_setup', 0 );
-function trt_plugin_setup () {
-    trt_register_post_type();
-    trt_register_taxonomy();
-    trt_register_post_meta();
+// Don't allow the plugin to activate if the full version is already active
+add_action( 'admin_init', 'trt_deactivate_if_full_version_is_active', 0 );
+function trt_deactivate_if_full_version_is_active () {
+    if ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'ttrpg-roll-tables/ttrpg-roll-tables.php' ) ) {
+        deactivate_plugins( 'ttrpg-roll-tables-starter-edition/ttrpg-roll-tables-starter-edition.php' );
+    }
 }
 
-// Load plugin CSS & JS
-add_action( 'wp_enqueue_scripts', 'trt_custom_scripts' );
-function trt_custom_scripts() {
+// Only proceed if the full version of the plugin isn't active
+if( ! function_exists( 'tttrpg_roll_tables_plugin_setup' ) ) {
+    // Include plugin files
+    include 'includes/post-types.php';
+    include 'includes/metabox.php';
+    include 'includes/ajax.php';
+    include 'includes/shortcode.php';
 
-    // Register and load the CSS
-    if( apply_filters( 'trt_use_plugin_css', true ) ) {
-        wp_register_style( 'trt-styles', plugins_url( 'assets/style.css', __FILE__ ), array(), '1.0' );
-        wp_enqueue_style( 'trt-styles' );
+    // Set up the plugin
+    add_action( 'init', 'tttrpg_roll_tables_plugin_setup', 0 );
+    function tttrpg_roll_tables_plugin_setup () {
+        trt_register_post_type();
+        trt_register_taxonomy();
+        trt_register_post_meta();
     }
 
-    // Regsiter the required JS - it will only be enqueued in the shortcode
-    wp_register_script( 'shuffle-letters', plugins_url( 'assets/jquery.shuffleLetters.js', __FILE__ ), array( 'jquery' ), '1.0' );
-    wp_register_script( 'trt-scripts', plugins_url( 'assets/scripts.js', __FILE__ ), array( 'jquery', 'shuffle-letters' ), '1.0' );
+    // Load plugin CSS & JS
+    add_action( 'wp_enqueue_scripts', 'trt_custom_scripts' );
+    function trt_custom_scripts() {
 
-    // Add ajax parameters and localised strings to the JS
-    wp_localize_script( 'trt-scripts', 'roll_table', 
-        array( 
-            'ajax_url' => admin_url( 'admin-ajax.php' ),
-            'nonce' => wp_create_nonce( 'roll-table-nonce' ),
-            'rolling' => apply_filters( 'trt_rolling_text', __( 'Rolling...', 'ttrpg-roll-tables' ) ),
-            'error_message' => apply_filters( 'trt_roll_error_text', __( 'Error - reload the page and roll again.', 'ttrpg-roll-tables' ) ),
-        )
-    );
+        // Register and load the CSS
+        if( apply_filters( 'trt_use_plugin_css', true ) ) {
+            wp_register_style( 'trt-styles', plugins_url( 'assets/style.css', __FILE__ ), array(), '1.0' );
+            wp_enqueue_style( 'trt-styles' );
+        }
+
+        // Regsiter the required JS - it will only be enqueued in the shortcode
+        wp_register_script( 'shuffle-letters', plugins_url( 'assets/jquery.shuffleLetters.js', __FILE__ ), array( 'jquery' ), '1.0' );
+        wp_register_script( 'trt-scripts', plugins_url( 'assets/scripts.js', __FILE__ ), array( 'jquery', 'shuffle-letters' ), '1.0' );
+
+        // Add ajax parameters and localised strings to the JS
+        wp_localize_script( 'trt-scripts', 'roll_table', 
+            array( 
+                'ajax_url' => admin_url( 'admin-ajax.php' ),
+                'nonce' => wp_create_nonce( 'roll-table-nonce' ),
+                'rolling' => apply_filters( 'trt_rolling_text', __( 'Rolling...', 'ttrpg-roll-tables' ) ),
+                'error_message' => apply_filters( 'trt_roll_error_text', __( 'Error - reload the page and roll again.', 'ttrpg-roll-tables' ) ),
+            )
+        );
+    }
 }
